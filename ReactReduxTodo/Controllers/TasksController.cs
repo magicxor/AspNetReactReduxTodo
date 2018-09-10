aa607037add9468b77db730cf850fb9966ee7769
Ilya Burakov <npravdin@gmail.com>
2018-09-11 04:08:14 +0800

    fix attribute routing

--- ReactReduxTodo/Controllers/TasksController.cs
+++ ReactReduxTodo/Controllers/TasksController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ReactReduxTodo.Controllers
 {
+    [Route("api/tasks")]
     public class TasksController : Controller
     {
         private readonly TodoTasksService _todoTasksService;
@@ -15,13 +16,13 @@ namespace ReactReduxTodo.Controllers
             _todoTasksService = todoTasksService;
         }
 
-        [HttpGet("/api/tasks")]
+        [HttpGet]
         public async Task<IEnumerable<TodoTask>> List()
         {
             return await _todoTasksService.ListAsync();
         }
 
-        [HttpGet("/api/tasks/{id}")]
+        [HttpGet("{id}")]
         public async Task<ActionResult<TodoTask>> Get(int id)
         {
             var model = await _todoTasksService.GetAsync(id);
@@ -32,14 +33,14 @@ namespace ReactReduxTodo.Controllers
             return model;
         }
 
-        [HttpPost("/api/tasks")]
+        [HttpPost]
         public async Task<ActionResult<int>> Add([FromBody] TodoTask todoTask)
         {
             var id = await _todoTasksService.AddAsync(todoTask);
             return id;
         }
 
-        [HttpDelete("/api/tasks/{id}")]
+        [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
             var success = await _todoTasksService.DeleteAsync(id);
