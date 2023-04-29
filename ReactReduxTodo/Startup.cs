256dc47e3fb5cd6fa84a4d05bb3aa222916244af
Ilia Burakov <burakov.ilia@gmail.com>
2023-04-30 00:04:38 +0800

    update .net and nuget packages

--- ReactReduxTodo/Startup.cs
+++ ReactReduxTodo/Startup.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
