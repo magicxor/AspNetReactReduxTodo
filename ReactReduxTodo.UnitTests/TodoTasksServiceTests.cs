582c49b16bf1e1cbd2bb6294ff4674e2c5b9f475
Ilia Burakov <magicxor@users.noreply.github.com>
2024-12-30 04:42:46 -0500

    update dependencies (#48)

--- ReactReduxTodo.UnitTests/TodoTasksServiceTests.cs
+++ ReactReduxTodo.UnitTests/TodoTasksServiceTests.cs
@@ -1,14 +1,12 @@
 using System.Data;
 using System.Data.Common;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using DotNet.Testcontainers.Builders;
-using DotNet.Testcontainers.Containers;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using ReactReduxTodo.Data;
 using ReactReduxTodo.Entities;
 using ReactReduxTodo.Services;
+using ReactReduxTodo.UnitTests.Constants;
+using ReactReduxTodo.UnitTests.Services;
 using Respawn;
 
 namespace ReactReduxTodo.UnitTests;
@@ -19,129 +17,39 @@ namespace ReactReduxTodo.UnitTests;
 [TestFixture]
 public class TodoTasksServiceTests
 {
-    private const string MsSqlDockerImage = "mcr.microsoft.com/mssql/server:2022-latest";
-    private const string MsSqlStartLogMessage = "The tempdb database has";
-    private static readonly TimeSpan DbStartTimeout = TimeSpan.FromMinutes(5);
-    private const string MsSqlPassword = "dev_pass_sm234Jm";
-    private const ushort MsSqlContainerPort = 1433;
-    private static readonly string MsSqlDockerContainerName = Guid.NewGuid().ToString("D");
-    
-    private ushort? _msSqlHostPort;
-    private string? _msSqlDockerConnectionString;
-    private IContainer? _dbContainer;
-    private DbConnection? _dbConnection;
     private Respawner? _respawner;
+    private MsSqlContainerManager? _dbContainerManager;
+    private DbConnection? _dbConnection;
     private DbContextOptions<ApplicationDbContext>? _dbContextOptions;
-
-    private static void WriteProgressMessage([StringSyntax(StringSyntaxAttribute.CompositeFormat)] string message, params object?[] arg)
-    {
-        TestContext.Progress.WriteLine(message, arg);
-    }
 
     [OneTimeSetUp]
     public async Task OneTimeSetUp()
     {
-        Trace.Listeners.Add(new ConsoleTraceListener());
-        
-        WriteProgressMessage($"Performing {nameof(OneTimeSetUp)}");
-        
-        _dbContainer = new ContainerBuilder()
-            .WithName(MsSqlDockerContainerName)
-            .WithImage(MsSqlDockerImage)
-            .WithExposedPort(MsSqlContainerPort)
-            .WithPortBinding(MsSqlContainerPort, true)
-            .WithEnvironment(new Dictionary<string, string>
-            {
-                {"MSSQL_IP_ADDRESS", "0.0.0.0"},
-                {"MSSQL_PID", "Developer"},
-                {"ACCEPT_EULA", "Y"},
-                {"MSSQL_SA_PASSWORD", MsSqlPassword},
-                {"PATH", "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"},
-            })
-            .WithWaitStrategy(Wait
-                .ForUnixContainer()
-                .UntilMessageIsLogged(MsSqlStartLogMessage))
-            .WithAutoRemove(true)
-            .WithCleanUp(true)
-            .Build();
-        
-        _dbContainer.Creating += DbContainerOnCreating;
-        _dbContainer.Created += DbContainerOnCreated;
-        _dbContainer.Starting += DbContainerOnStarting;
-        _dbContainer.Started += DbContainerOnStarted;
-        _dbContainer.Stopping += DbContainerOnStopping;
-        _dbContainer.Stopped += DbContainerOnStopped;
-        
-        using (var cancellationTokenSource = new CancellationTokenSource(DbStartTimeout))
-        {
-            var cancellationToken = cancellationTokenSource.Token;
-            await _dbContainer.StartAsync(cancellationToken);
-        }
+        _dbContainerManager = new MsSqlContainerManager();
+        var containerInfo = await _dbContainerManager.StartAsync();
 
-        _msSqlHostPort = _dbContainer.GetMappedPublicPort(MsSqlContainerPort);
-        _msSqlDockerConnectionString =
-            $"Server=tcp:localhost,{_msSqlHostPort};Encrypt=False;Database=AspNetReactReduxTodo;MultipleActiveResultSets=true;User ID=SA;Password={MsSqlPassword}";
-        
+        var msSqlDockerConnectionString =
+            $"Server=tcp:{containerInfo.Host},{containerInfo.Port};Encrypt=False;Database=AspNetReactReduxTodo;MultipleActiveResultSets=true;User ID=SA;Password={Defaults.DbPassword}";
+
         _dbContextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseSqlServer(_msSqlDockerConnectionString)
+            .UseSqlServer(msSqlDockerConnectionString)
             .Options;
         await using var dbContext = new ApplicationDbContext(_dbContextOptions);
         await dbContext.Database.MigrateAsync();
 
-        _dbConnection = new SqlConnection(_msSqlDockerConnectionString);
+        _dbConnection = new SqlConnection(msSqlDockerConnectionString);
         await _dbConnection.OpenAsync();
 
         _respawner = await Respawner.CreateAsync(_dbConnection, new RespawnerOptions
         {
             DbAdapter = DbAdapter.SqlServer,
-            SchemasToInclude = new[] {"dbo"},
+            SchemasToInclude = ["dbo"],
         });
-    }
-    
-    private void LogWithContainerInfo(string message)
-    {
-        WriteProgressMessage(message + " host port = {0}, container port = {1}, image = {2}, container name = {3}", 
-            _msSqlHostPort,
-            MsSqlContainerPort,
-            MsSqlDockerImage,
-            MsSqlDockerContainerName);
-    }
-
-    private void DbContainerOnStopped(object? sender, EventArgs e)
-    {
-        LogWithContainerInfo("DB container stopped:");
-    }
-
-    private void DbContainerOnStopping(object? sender, EventArgs e)
-    {
-        LogWithContainerInfo("Stopping DB container:");
-    }
-
-    private void DbContainerOnStarted(object? sender, EventArgs e)
-    {
-        LogWithContainerInfo("DB container started:");
-    }
-
-    private void DbContainerOnStarting(object? sender, EventArgs e)
-    {
-        LogWithContainerInfo("Starting DB container:");
-    }
-
-    private void DbContainerOnCreated(object? sender, EventArgs e)
-    {
-        LogWithContainerInfo("DB container created:");
-    }
-
-    private void DbContainerOnCreating(object? sender, EventArgs e)
-    {
-        LogWithContainerInfo("Creating DB container:");
     }
 
     [OneTimeTearDown]
     public async Task OneTimeTearDown()
     {
-        WriteProgressMessage($"Performing {nameof(OneTimeTearDown)}");
-        
         _respawner = null;
         
         if (_dbConnection != null)
@@ -154,19 +62,14 @@ public class TodoTasksServiceTests
             await _dbConnection.DisposeAsync();
         }
 
-        if (_dbContainer != null)
+        if (_dbContainerManager != null)
         {
-            await _dbContainer.StopAsync();
-            await _dbContainer.DisposeAsync();
+            await _dbContainerManager.StopAsync();
         }
-        
-        Trace.Flush();
     }
 
     private async Task ResetDatabaseAsync()
     {
-        WriteProgressMessage($"Performing {nameof(ResetDatabaseAsync)}");
-        
         if (_respawner != null && _dbConnection != null)
         {
             await _respawner.ResetAsync(_dbConnection);
