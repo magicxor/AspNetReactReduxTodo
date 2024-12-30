cb9ad941125e35f026f6506fedf8f4cb8a4f7d6b
Ilia Burakov <burakov.ilia@gmail.com>
2024-12-30 06:21:35 -0500

    remove deprecated code

--- ClientApp/src/store/configureStoreCustom.js
+++ ClientApp/src/store/configureStoreCustom.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { reducer as TaskListReducer } from '../features/taskList/reducer';
 import createSagaMiddleware from 'redux-saga';
 import { sagas } from '../sagas';
@@ -8,28 +8,18 @@ export function configureStoreCustom(initialState) {
     taskList: TaskListReducer,
   });
 
-  // create the saga middleware
   const sagaMiddleware = createSagaMiddleware();
-
-  // In development, use the browser's Redux dev tools extension if installed
-  const enhancers = [];
   const isDevelopment = process.env.NODE_ENV === 'development';
-  if (
-    isDevelopment &&
-    typeof window !== 'undefined' &&
-    window.devToolsExtension
-  ) {
-    enhancers.push(window.devToolsExtension());
-  }
 
-  const store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(sagaMiddleware), ...enhancers),
-  );
+  const store = configureStore({
+    reducer: rootReducer,
+    preloadedState: initialState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
+    devTools: isDevelopment,
+  });
 
-  // then run the saga
-  sagas.map((saga) => sagaMiddleware.run(saga));
+  sagas.forEach((saga) => sagaMiddleware.run(saga));
 
   return store;
 }
