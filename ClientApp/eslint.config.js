a3b44bcae4870915acc77d8e94c63f522c7e92a8
Ilia Burakov <magicxor@users.noreply.github.com>
2025-11-23 14:06:56 -0500

    Adds linting to CI workflow (#68)
    * Updates ESLint configuration for improved linting
    
    Refactors the ESLint configuration to enhance code quality checks.
    
    Removes Prettier integration from ESLint and integrates stylistic rules directly.
    
    Adds Jest and Vitest testing environment configurations with globals.
    Specifies dedicated rules for testing and source files.
    
    * Adds linting to the CI workflow
    
    Ensures code quality by running the linter as part of the CI process.
    
    This helps to identify and fix code style issues before deployment, improving maintainability and reducing potential errors.

--- ClientApp/eslint.config.js
+++ ClientApp/eslint.config.js
@@ -3,8 +3,9 @@ import globals from 'globals';
 import reactPlugin from 'eslint-plugin-react';
 import hooksPlugin from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import prettierPlugin from 'eslint-plugin-prettier';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import stylistic from '@stylistic/eslint-plugin';
+import vitest from '@vitest/eslint-plugin';
+import jest from 'eslint-plugin-jest';
 
 const ignore = [
   'build/**',
@@ -16,18 +17,100 @@ const ignore = [
   'OidcTrustedDomains.js',
 ];
 
-const files = ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'];
+const files = ['src/*.js', 'src/*.jsx', 'src/*.ts', 'src/*.tsx'];
+
+const testFiles = ['tests/*.js', 'tests/*.ts', 'src/**/__tests__/*.{js,jsx,ts,tsx}'];
 
 export default [
   {
-    files: files,
-    ignores: ignore,
-    ...eslint.configs.recommended,
+    files: testFiles,
+    ...jest.configs['flat/recommended'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': hooksPlugin,
+      'react-refresh': reactRefresh,
+      '@stylistic': stylistic,
+      vitest,
+      jest,
+    },
+    rules: {
+      ...eslint.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...hooksPlugin.configs.recommended.rules,
+      ...reactRefresh.configs.recommended.rules,
+      ...vitest.configs.recommended.rules,
+      ...jest.configs['flat/recommended'].rules,
+      'jest/prefer-expect-assertions': 'off',
+      'jest/expect-expect': ['warn', { assertFunctionNames: ['expect', 'expectSaga'] }],
+      'vitest/expect-expect': ['warn', { assertFunctionNames: ['expect', 'expectSaga'] }],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'max-len': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/function-component-definition': 'off',
+      'no-restricted-syntax': 'off',
+      'no-plusplus': 'off',
+      'no-await-in-loop': 'off',
+      'no-continue': 'off',
+      'no-underscore-dangle': 'off',
+      'class-methods-use-this': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/jsx-curly-brace-presence': 'warn',
+      'react/self-closing-comp': 'warn',
+      'react/require-default-props': 'off',
+      'import/extensions': 'off',
+      'react/prop-types': 'off',
+      'react/destructuring-assignment': 'off',
+      // imports
+      'import/prefer-default-export': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'import/order': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      // indents
+      'react/jsx-indent': 'off',
+      // spaces
+      'spaced-comment': 'warn',
+      'no-trailing-spaces': 'warn',
+      // lines
+      'implicit-arrow-linebreak': 'off',
+      'function-paren-newline': 'off',
+      'object-curly-newline': 'off',
+      'no-multiple-empty-lines': 'warn',
+      'react/jsx-props-no-multi-spaces': 'warn',
+      // conditions
+      'no-else-return': 'off',
+      'no-nested-ternary': 'off',
+      // functions
+      'arrow-body-style': 'off',
+      'arrow-parens': 'off',
+    },
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...vitest.globals,
+        ...vitest.environments.env.globals,
+        ...jest.globals,
+      },
+    },
   },
   {
     files: files,
     ignores: ignore,
-    ...eslintConfigPrettier,
+    ...eslint.configs.recommended,
   },
   {
     files: files,
@@ -41,20 +124,14 @@ export default [
       react: reactPlugin,
       'react-hooks': hooksPlugin,
       'react-refresh': reactRefresh,
-      prettier: prettierPlugin,
-      eslintConfigPrettier: eslintConfigPrettier,
+      '@stylistic': stylistic,
     },
     rules: {
       ...eslint.configs.recommended.rules,
       ...reactPlugin.configs.recommended.rules,
       ...hooksPlugin.configs.recommended.rules,
       ...reactRefresh.configs.recommended.rules,
-      ...prettierPlugin.configs.recommended.rules,
-      'prettier/prettier': 'error',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'max-len': 'off',
       'react/jsx-props-no-spreading': 'off',
       'react/function-component-definition': 'off',
